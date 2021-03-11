5c4ae043a7b92e6853452c1d413046ff92bd7eae
Aruni Mishra <arunimishramsit@gmail.com>
2021-03-11 10:52:37 +0530

    spring Autowired

--- src/main/java/com/example/firstSpringProject/FirstSpringProjectApplication.java
+++ src/main/java/com/example/firstSpringProject/FirstSpringProjectApplication.java
@@ -2,12 +2,16 @@ package com.example.firstSpringProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class FirstSpringProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstSpringProjectApplication.class, args);
-	}
+		ApplicationContext context= SpringApplication.run(FirstSpringProjectApplication.class, args);
 
+		Alien obj = context.getBean(Alien.class);
+
+		obj.code();
+	}
 }
---
+++ src/main/java/com/example/firstSpringProject/Laptop.java
@@ -0,0 +1,12 @@
+package com.example.firstSpringProject;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+
+    public void compile() {
+        System.out.println("laptop is compiling...");
+    }
+
+}
